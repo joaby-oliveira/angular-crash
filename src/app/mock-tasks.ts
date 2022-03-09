ccaf4eff849426f90a7778913c76f254073fac75
Joaby Oliveira <joabyoliveira2004@gmail.com>
2022-03-09 00:11:05 -0300

    feat: make loop of tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Lavar a roupa',
+    day: '5 de Maio 15:30',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Reunião na firma',
+    day: '5 de Maio 16:30',
+    reminder: true,
+  },
+  {
+    id: 1,
+    text: 'Estudar node e angular',
+    day: '5 de Maio 17:30',
+    reminder: true,
+  },
+];
